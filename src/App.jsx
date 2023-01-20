5c3c0b2d1affb94ea5c03ba571d08489fe7bd327
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-01-20 19:27:26 -0300

    Recriado projeto com Vite (React + SWC)

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+        <div className="navbar mx-auto mb-80 border border-1">
+            <ul className="botoes-navbar">
+                <li className="mr-auto"><h1 className="d-inline logotipo">Leonardo</h1></li>
+                <li className="ml-auto btn">Experiência</li>
+                <li className="btn">Formação</li>
+                <li className="btn">Contato</li>
+            </ul>
+        </div>
+        <header className="d-flex">
+            <img
+                className="col-4 border-radius-5"
+                src="img/JX3A6225.jpg"
+                alt="Minha foto"
+                width="300px"
+            />
+            <div className="col-6 pl-140">
+                <h1 className="desenvolvedor color-g12">Desenvolvedor Full Stack</h1>
+                <h2 className="localizacao">Localização: Curitiba, Paraná, Brasil</h2>
+            </div>
+        </header>
+        <div id="experiencia">
+            <div className="d-flex">
+                <div className="col-5"></div>
+                <p className="texticulo color-g9 col-7 float-right max-width-3col">
+                    Desenvolvo pequenos projetos utilizando
+                    <span className="text-bold"
+                    >HTML, Javascript, CSS, PHP, Spring Boot.</span
+                    >
+                </p>
+            </div>
+
+            <div className="d-flex float-right">
+                <div className="when col-5 text-align-right">2021</div>
+                <div className="what bg-g3 border-g5 col-7 max-width-3col">
+                    <div className="primeira-coluna">
+                        <div className="card-title color-g10">Pílulas do SUS</div>
+                        <div className="card-text color-g10">Healthcare blog.</div>
+                    </div>
+                    <div className="segunda-coluna">
+                        <div className="job-title color-g10">Developer</div>
+                        <ul className="caixinhas">
+                            <li className="caixinha">HTML</li>
+                            <li className="caixinha">CSS</li>
+                            <li className="caixinha">UI Design</li>
+                            <li className="caixinha">Wordpress</li>
+                            <li className="caixinha">Instagram</li>
+                            <li className="caixinha">Facebook</li>
+                            <li className="caixinha">Author</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+    )
+}
+
+export default App
