a2e845cb267df96727a6c8faff0027ccac9a4407
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-01-27 15:05:30 -0300

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
             <Portfolio/>
             <Formacao/>
             <Contato/>
+            <div className="mb-[120px]"></div>
         </>
     )
 }
--- src/Contato.jsx
+++ src/Contato.jsx
@@ -1,12 +1,12 @@
 export function Contato() {
     return (
         <div id="contato"
-             className="font-fira prose text-gray-g12 md:text-4xl md:leading-[60px] flex flex-col items-end gap-y-7 lg:grid
-             lg:grid-cols-12 lg:gap-10 lg:items-stretch mb-[120px]">
-            <p className="lg:col-start-1 lg:col-span-6">
+             className="font-fira flex flex-col items-end gap-y-7 md:grid
+             md:grid-cols-12 md:gap-10 md:items-stretch text-gray-g12">
+            <p className="md:col-start-1 md:col-span-6 prose md:prose-xl lg:prose-2xl">
                 Estou disponível para novos projetos. Por favor, entre em contato.
             </p>
-            <p className="lg:col-end-13 lg:col-span-6 lg:text-right">
+            <p className="md:col-end-13 md:col-span-6 md:text-right prose md:prose-xl lg:prose-2xl">
                 <a href="mailto:leonardo@vilelapinheiro.com" target="_blank">leonardo@vilelapinheiro.com</a><br/>
                 <a href="https://wa.me/5543996502757" target="_blank">+55 43 9-9650-2757</a>
             </p>
